fb569ca313cc3946cfa536d34b9f8deda2350f71
gkholman <gkenholman+github@gmail.com>
2021-01-16 15:27:17 -0500

    Raw conversion from CS01 Word to DocBook XML.

---
+++ produceGenericode.bat
@@ -0,0 +1,34 @@
+@echo off
+
+if not "a%~4" == "a" goto :argsokay  
+echo Missing results directory, platform, label, date-stamp, user, password arguments
+exit /B 1
+:argsokay
+
+if exist "%~1\." goto :resultokay
+echo Cannot find result directory "%~1".
+echo (perhaps the argument is ending in "/" when it is not supposed to)
+exit /B 1
+:resultokay
+
+if exist "hub.console.%~3.txt" del "hub.console.%~3.txt"
+
+set package=genericode-1.0-v1.0
+set thisStage=csd04
+set prevStage=csd03
+set label=%~3
+
+echo Building package...
+java "-Dant.home=utilities/ant" -classpath "db/spec-0.8/validate/xjparse.jar;utilities/ant/lib/ant-launcher.jar;db/spec-0.8/validate/saxon9he.jar;." org.apache.tools.ant.launch.Launcher -buildfile produceGenericode.xml "-Ddir=%~1" "-Dstage=%thisStage%" "-DprevStage=%prevStage%" "-Dpackage=%package%" "-Dlabel=%label%" "-Ddatetimelocal=%~4" "-Drealtauser=%~5" "-Drealtapass=%~6" >"hub.console.%label%.txt"
+set serverReturn=%errorlevel%
+
+if not exist "%~1\" mkdir "%~1" 2>&1
+if not exist "%~1\%package%-%thisStage%-%label%\" mkdir "%~1\%package%-%thisStage%-%label%" 2>&1
+if not exist "%~1\%package%-%thisStage%-%label%\archive-only-not-in-final-distribution\" mkdir "%~1\%package%-%thisStage%-%label%\archive-only-not-in-final-distribution" 2>&1
+
+move "hub.console.%label%.txt" "%~1\%package%-%thisStage%-%label%\archive-only-not-in-final-distribution"
+echo %serverReturn% >"%~1\%package%-%thisStage%-%label%\archive-only-not-in-final-distribution\hub.exit.%label%.txt"
+
+pushd "%~1"
+7z a "%package%-%thisStage%-%label%.zip" "%package%-%thisStage%-%label%"
+popd
---
+++ produceGenericode.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ -f hub.console.$3.txt ]; then rm hub.console.$3.txt ; fi
+
+if [ "$4" = "" ] || ( [ "$5" != "" ] && [ "$6" = "" ] ); then echo Missing results directory, platform, label, date-stamp, user, and password arguments ; exit 1 ; fi
+
+package=genericode-1.0-v1.0
+thisStage=csd04
+prevStage=csd03
+label=$3
+
+echo Building package for $package $thisStage...
+java -Dant.home=utilities/ant -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile produceGenericode.xml -Ddir=$1 -Dstage=$thisStage -DprevStage=$prevStage -Dpackage=$package -Dlabel=$label -Ddatetimelocal=$4 -Drealtauser=$5 -Drealtapass=$6 | tee hub.console.$3.txt
+serverReturn=${PIPESTATUS[0]}
+echo $serverReturn >hub.exitcode.$3.txt
+
+if [ -f $1/$package-$thisStage-$label-archive-only.zip ]
+then
+7z a $1/$package-$thisStage-$label-archive-only.zip hub.console.$3.txt hub.exitcode.$3.txt
+fi
+
+if [ ! -d $1 ]; then mkdir $1 ; fi
+if [ ! -d $1/$package-$thisStage-$label ]; then mkdir $1/$package-$thisStage-$label ; fi
+if [ ! -d $1/$package-$thisStage-$label/archive-only-not-in-final-distribution/ ]; then mkdir $1/$package-$thisStage-$label/archive-only-not-in-final-distribution/ ; fi
+mv  hub.console.$3.txt $1/$package-$thisStage-$label/archive-only-not-in-final-distribution/
+mv hub.exitcode.$3.txt $1/$package-$thisStage-$label/archive-only-not-in-final-distribution/
+
+# reduce GitHub storage costs by zipping results and deleting intermediate files
+pushd $1
+7z a $package-$thisStage-$label.zip $package-$thisStage-$label
+popd
+
+if [ "$1" = "target" ]
+then
+if [ "$2" = "github" ]
+then
+if [ "$7" = "DELETE-REPOSITORY-FILES-AS-WELL" ] #secret undocumented failsafe
+then
+# further reduce GitHub storage costs by deleting repository files
+
+find . -not -name target -not -name .github -maxdepth 1 -exec rm -r -f {} \;
+mv $1/$package-$thisStage-$label.zip .
+rm -r -f $1
+
+fi
+fi
+fi
+
+exit 0 # always be successful so that github returns ZIP of results
+
