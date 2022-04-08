edaf9b3f95525e1172cca93fe5df79fb46d547b1
gkholman <gkenholman+github@gmail.com>
2022-04-08 09:52:28 -0400

    CS02 first draft responding to editing instructions

--- produceGenericode.sh
+++ produceGenericode.sh
@@ -5,7 +5,7 @@ if [ -f build.console.$3.txt ]; then rm build.console.$3.txt ; fi
 if [ "$3" = "" ] || ( [ "$4" != "" ] && [ "$5" = "" ] ); then echo Missing results directory, platform, label, user, and password arguments ; exit 1 ; fi
 
 package=genericode-v1.0
-thisStage=csd05
+thisStage=cs02
 prevStage=csd04
 targetdir=$1
 platform=$2
