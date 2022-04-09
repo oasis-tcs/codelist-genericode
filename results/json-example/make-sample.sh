262fe8ec9dec8ba1da0cf7296640ac20657c0a2c
gkholman <gkenholman+github@gmail.com>
2022-04-08 20:27:02 -0400

    CS02 cover repairs

--- results/json-example/make-sample.sh
+++
@@ -1,4 +0,0 @@
-saxon9he -xsl:../xslt/gc2gcj.xsl -s:../xml/CaseTypeCode.gc -o:CaseTypeCode.gcj
-saxon9he -xsl:../xslt/gc2gcj.xsl -s:../xml/ChannelCode-2.3.gc -o:ChannelCode-2.3.gcj
-saxon9he -xsl:../xslt/gc2gcj.xsl -s:../xml/CurrencyCode-2.3.gc -o:CurrencyCode-2.3.gcj
-saxon9he -xsl:../xslt/gc2gcj.xsl -s:../xml/UnitOfMeasureCode-2.3.gc -o:UnitOfMeasureCode-2.3.gcj
